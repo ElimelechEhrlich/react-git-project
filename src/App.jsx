d56bc25c3c22b6ccc85316422bbcb8b77323a326
Elimelech Ehrlich <ee525566@gmail.com>
2026-02-23 10:28:12 +0200

    create grid component logic and Node component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-function App() {
-  return (
-    <>
-    <div>app</div>
-    </>
+import React from 'react'
+import GridLayout from './GridLayout'
+
+export default function App() {
+  return (<>
+  <div className='background'>
+    <GridLayout/>
+  </div>
+  </>
   )
 }
 
-export default App
+
--- src/GridLayout.jsx
+++ src/GridLayout.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from "react";
+import Node from "./Node";
 
 export default function GridLayout() {
+    const grid = [];
+  for (let row = 0; row < 9; row++) {
+    const currentRow = [];
+    for (let col = 0; col < 10; col++) {
+      currentRow.push(new Node);
+    }
+    grid.push(currentRow);
+  }
   return (
-    <div>GridLayout</div>
+    <div className="grid">
+      {
+        grid.map((row, rowId) => {
+          return(
+            <div className="row" key={rowId}>
+
+              {
+                row.map((node, nodeId) => {
+                  return (
+                    <Node></Node>
+                  );
+                })
+              }
+              </div>
+          )
+        })
+      }
+    </div>
   )
 }
