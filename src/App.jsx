b6e96ad8c42c84a5a0c6357919a27fd798d2bfce
Elimelech Ehrlich <ee525566@gmail.com>
2026-02-23 15:09:26 +0200

    create the other elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
-import GridLayout from './GridLayout'
-import Cell from './Cell'
+import React, { useRef, useState } from "react";
+import GridLayout from "./GridLayout";
+import Cell from "./Cell";
+import { useClickHendler } from "./clickHendler";
+import { useSecret } from "./useSecret";
 
 export default function App() {
-  return (<>
-  <div className='background justify-around flex flex-col'>
-    <div className='flex flex-row header'>
-      <p>Find the </p>
-      <p>Secret Color!</p>
-      <Cell key={'1000'} className='cell_header'/>
-    </div>
-    <GridLayout/>
-  </div>
-  </>
-  )
+
+  return (
+    <>
+      <div className="background justify-around flex flex-col">
+        <div className="flex flex-row header">
+          <p>Find the </p>
+          <p>Secret Color!</p>
+          <Cell key={"1000"} className="cell_header" />
+        </div>
+        <GridLayout  />
+        <p>
+          Clicks: 
+        </p>
+        <p>Keep Searching!</p>
+      </div>
+    </>
+  );
 }
