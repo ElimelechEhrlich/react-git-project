3d45ae9add9e9d9e0af69673fb203feb6c1971c2
Elimelech Ehrlich <ee525566@gmail.com>
2026-02-23 17:05:47 +0200

    fixed styling

--- src/App.jsx
+++ src/App.jsx
@@ -12,17 +12,17 @@ export default function App() {
   return (
     <>
       <div className="background justify-around flex flex-col">
-          <p style={{margin:'0', color: "rgb(255, 255, 255)", fontSize:'35px', fontWeight: 'bold'}}>Welcome to the game!</p>
+          <p style={{margin:'0', color: "rgb(255, 255, 255)", fontSize:'35px', fontFamily: 'Gill Sans, sans-serif', fontWeight: 'bold'}}>Welcome to the game!</p>
         <div className="flex flex-row header">
-          <p style={{margin:'0', color: 'rgb(222, 92, 95)', fontSize:'35px',   fontWeight: 'bold'}}>Find the color:</p>
+          <p style={{margin:'0', color: 'rgb(222, 92, 95)', fontSize:'35px', fontFamily: 'Gill Sans, sans-serif',  fontWeight: 'bold'}}>Find the color:</p>
           <Cell key={"1000"} bgColor={colorRef.current} className="cell_header" />
         </div>
         <GridLayout event={onclick} />
         <div style={{margin:'0'}}>
-        <p style={{margin:'0', color: "rgb(255, 255, 255)", fontSize:'35px', fontWeight: 'bold', justifySelf:'center'}}>
+        <p style={{margin:'0', color: "rgb(255, 255, 255)", fontSize:'35px', fontFamily: 'Gill Sans, sans-serif', fontWeight: 'bold', justifySelf:'center'}}>
           Clicks: <span style={{margin:'0', fontSize:'45px', fontWeight: 'bold'}}>{clicksCounter}</span>
         </p>
-        <p style={{margin:'0', color: 'rgb(252, 210, 79)', fontSize:'35px', fontWeight: 'bold', justifySelf:'center'}}>Keep Searching!</p>
+        <p style={{margin:'0', color: `${!isGameOver? 'rgb(252, 210, 79)': 'rgb(4, 241, 68)'}`, fontSize:'35px', fontFamily: 'Gill Sans, sans-serif', fontWeight: 'bold', justifySelf:'center'}}>{!isGameOver? 'Keep Searching!': 'You found the secret color!'}</p>
         </div>
       </div>
     </>
--- src/Cell.jsx
+++ src/Cell.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Cell({ id, className, event, bgColor }) {
-  return <div onClick={event} id={id} style={{backgroundColor: `${bgColor}`}} className={className}></div>;
+  return <div onClick={event} id={id} style={{backgroundColor: `${bgColor}`,   boxShadow: '0.5px 0.5px 1px black'}} className={className}></div>;
 }
